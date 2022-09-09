131b87ef0be45c213b38ccf09d99354fc1724135
Muhammad Syarif Januriansyah <msyarifjanuriansyah@gmail.com>
2022-09-09 12:45:31 +0700

    Revert "update migration"
    This reverts commit dc2964c58cf92f12d20f03f1e4a1d57715742ec9.

--- database/migrations/2022_09_07_064857_create_crm_leads_fu_table.php
+++ database/migrations/2022_09_07_064857_create_crm_leads_fu_table.php
@@ -14,15 +14,8 @@ class CreateCrmLeadsFuTable extends Migration
     public function up()
     {
         Schema::create('crm_leads_fu', function (Blueprint $table) {
-            $table->id('id_leads_fu');
-            $table->integer('id_customer');
-            $table->date("time_leads");
-            $table->text('note')->nullable()->default(null);
-            $table->text('note_spv')->nullable()->default(null);
-            $table->text('note_manager')->nullable()->default(null);
-            $table->text('note_kendala')->nullable()->default(null);
-            $table->string('rencana_fu',100)->nullable()->default(null);
-            $table->date('registered');
+            $table->id();
+            $table->timestamps();
         });
     }
 
